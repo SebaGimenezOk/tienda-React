e0327b7345be7fff81711ca403aadc3163331cce
SebaGimenez <sebastiangimenezlopezen@gmail.com>
2022-07-31 19:55:12 -0300

    ajsutes css

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -3,16 +3,25 @@ import Card from 'react-bootstrap/Card';
 import ItemCount from '../ItemCount/ItemCount';
 
 
-function ItemDetail({ datos,action }) {
-    const { title, price, img, stock } = datos;
+function ItemDetail({ datos, action }) {
+    const { title, price, img, stock, description } = datos;
 
     return (
-        <Card className="Item-Product">
-            <img className="caja-foto" src={`./assets/${img}`} alt="imagen producto" />
-            <p className="P-Products">{title}</p>
-            <span className='P-Precios'>$ {price} </span>
-            <p className='P-Precios'> en stock: {stock} </p>
-            <ItemCount/>
+        <Card>
+            <div className='Hero-Container'>
+                <div className='Item-Uno'>
+                    <img className="caja-foto" src={`./assets/${img}`} alt="imagen producto" />
+                    <div className=''>
+                        <p className="P-Products">{title}</p>
+                        <span className='P-Precios'>por kilo: $ {price}</span>
+                    </div>
+                </div>
+                <div className='Item-Dos'>
+                    <p> {description} </p>
+                    <p className='P-Precios'> en stock: {stock} </p>
+                    <ItemCount />
+                </div>
+            </div>
         </Card>
     );
 }
--- src/components/utils/articulo.mock.js
+++ src/components/utils/articulo.mock.js
@@ -6,20 +6,25 @@ const artic = [
         price: 1700,
         img:'holanda.png',
         stock: 20,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non vehicula dolor, vestibulum tempor diam. Nulla elementum facilisis lacinia. Curabitur vulputate magna est, quis tincidunt metus efficitur sit amet. Fusce neque odio, accumsan a facilisis at, dapibus nec arcu. Quisque eget libero nec metus vehicula aliquet nec in lorem. Nullam posuere est sapien. Fusce quis ex a lectus vehicula pellentesque ut vitae nulla. Proin ultrices eu nunc at posuere.', 
     },
-    {
+        {
         id:"2",
         title: "cremoso",
         price: 1400,
         img:'cremosoComun.png',
         stock: 20,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non vehicula dolor, vestibulum tempor diam. Nulla elementum facilisis lacinia. Curabitur vulputate magna est, quis tincidunt metus efficitur sit amet. Fusce neque odio, accumsan a facilisis at, dapibus nec arcu. Quisque eget libero nec metus vehicula aliquet nec in lorem. Nullam posuere est sapien. Fusce quis ex a lectus vehicula pellentesque ut vitae nulla. Proin ultrices eu nunc at posuere.', 
+
     },
     {
         id:"3",
-        title: "reggiano",
+        title: "Queso Reggiano",
         price: 2100,
         img:'reggiano.png',
         stock: 20,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non vehicula dolor, vestibulum tempor diam. Nulla elementum facilisis lacinia. Curabitur vulputate magna est, quis tincidunt metus efficitur sit amet. Fusce neque odio, accumsan a facilisis at, dapibus nec arcu. Quisque eget libero nec metus vehicula aliquet nec in lorem. Nullam posuere est sapien. Fusce quis ex a lectus vehicula pellentesque ut vitae nulla. Proin ultrices eu nunc at posuere.', 
+
     },
     {
         id:"4",
@@ -27,6 +32,8 @@ const artic = [
         price: 1900,
         img:'sardo.png',
         stock: 20,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non vehicula dolor, vestibulum tempor diam. Nulla elementum facilisis lacinia. Curabitur vulputate magna est, quis tincidunt metus efficitur sit amet. Fusce neque odio, accumsan a facilisis at, dapibus nec arcu. Quisque eget libero nec metus vehicula aliquet nec in lorem. Nullam posuere est sapien. Fusce quis ex a lectus vehicula pellentesque ut vitae nulla. Proin ultrices eu nunc at posuere.', 
+
     },
     ]
     
